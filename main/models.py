5be8f9ae0b78dda234dc19e7a71dbdfe858ca2cc
Farhod <farkhodismailov0308@gmail.com>
2026-02-20 22:12:29 +0500

    Manzil va telefon maydonlari qo'shildi

--- main/models.py
+++ main/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Maqola(models.Model):
     sarlavha = models.CharField(max_length=200)
     matn = models.TextField()
-class Maqola(models.Model):
-    sarlavha = models.CharField(max_length=200)
-    matn = models.TextField()
-    # Yangi maydon:
     rasm = models.ImageField(upload_to='maqola_rasmlari/', blank=True, null=True)
     yaratilgan_sana = models.DateTimeField(auto_now_add=True)
+    
+    # Yangi qo'shilgan maydonlar:
+    manzil = models.CharField(max_length=255, blank=True, null=True, default="Noma'lum")
+    telefon = models.CharField(max_length=20, blank=True, null=True, default="Ko'rsatilmagan")
 
     def __str__(self):
         return self.sarlavha
