aab392d60d33b6a1145db98bb02fa1e564eefeb2
Farhod <farkhodismailov0308@gmail.com>
2026-02-21 13:19:10 +0500

    Settings: WhiteNoise storage added

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
